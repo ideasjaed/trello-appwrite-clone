cf26c51dea91035447331232be4696e0cfe58d98
ideasjaed <ideas.jaed@gmail.com>
2023-05-26 16:16:41 -0600

    form

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ function Header() {
             height={100}
             className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
             />
-            <div className="">
+            <div className="flex items-center space-x-5 flex-1 justify-end">
                 <form className="flex items-center space-x-5 bg-white rounded-md shadow-md flex-1 md:flex-initial">
                     <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
                     <input type="text" placeholder="Search" name="" id="" className="flex-1 outline-none p-2" />
@@ -25,6 +25,12 @@ function Header() {
                 </form>
                 <Avatar name="Jazmin Lino" round color="#0055D1" size="50" />
             </div>
+        </div>
+        <div className="flex items-center justify-center px-5 md:py5">
+            <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
+                <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] r-1" />
+                GPT is summarising your tasks for the day...
+            </p>
         </div>
     </header>
   )
